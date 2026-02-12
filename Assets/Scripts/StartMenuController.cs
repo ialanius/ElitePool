3193b5e7e5ae6eb8d924614125c6b410171b5765
Ālanius <ialaniius@gmail.com>
2026-02-12 15:01:57 +0300

    Updated Interfaces & Aim line

---
+++ Assets/Scripts/StartMenuController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private string mainMenuSceneName = "MainMenu";
+    // دالة زر البدء
+    public void PlayGame()
+    {
+        // 1. إجبار الصوت على العمل (لحل مشكلة الويب)
+        AudioListener.pause = false; // إلغاء الإيقاف المؤقت للصوت
+        AudioListener.volume = 1.0f; // التأكد أن الصوت مرفوع 100%
+
+        // 2. التأكد من أن الزمن يعمل (في حال توقف بسبب كود التدوير السابق)
+        Time.timeScale = 1;
+
+        // 3. الانتقال للمشهد التالي
+        SceneManager.LoadScene(mainMenuSceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
